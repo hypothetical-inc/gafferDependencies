10b4d3f717f150a15414022f631ca7878db6bb70
Alex Fuller <boberfly@gmail.com>
2017-09-17 13:58:03 -0700

    zlib-1.2.11

--- winbuild/buildZLIB.bat
+++ winbuild/buildZLIB.bat
@@ -1,13 +1,20 @@
-cd %~dp0%..\zlib-1.2.11
+SETLOCAL
+
+cd %ROOT_DIR%\zlib-1.2.11
 
 mkdir %BUILD_DIR%\doc\licenses
 copy LICENSE %BUILD_DIR%\doc\licenses\libzlib
 
 mkdir gafferBuild
 cd gafferBuild
-del CMakeCache.txt
+del /f CMakeCache.txt
 
 cmake -Wno-dev -G %CMAKE_GENERATOR% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% ..
+if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 cmake --build . --config %BUILD_TYPE% --target install
+if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
-cd %ROOT_DIR%
+rem This is silly, why does zlib move zconf.h and fails Boost building?
+move ..\\zconf.h.included ..\\zconf.h
+
+ENDLOCAL
