4400e4c48f752e99f4d3ac8f46139a8509eafd23
Alex Fuller <boberfly@gmail.com>
2018-05-26 11:12:10 -0700

    OpenSSL lib builds are in a slightly different location for alternate generators

--- winbuild/buildOpenSSL.bat
+++ winbuild/buildOpenSSL.bat
@@ -9,13 +9,18 @@ mkdir gafferBuild
 cd gafferBuild
 del /f CMakeCache.txt
 
-cmake -Wno-dev -G %CMAKE_GENERATOR% -DBUILD_OBJECT_LIBRARY_ONLY=ON -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% ..
+cmake -Wno-dev -G %CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DBUILD_OBJECT_LIBRARY_ONLY=ON -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% ..
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
-cmake --build . --config %BUILD_TYPE% --target all_build
+cmake --build . --config %BUILD_TYPE% --target all -- -j %NUMBER_OF_PROCESSORS%
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
-copy crypto\%BUILD_TYPE%\crypto.lib %BUILD_DIR%\lib
-copy ssl\%BUILD_TYPE%\ssl.lib %BUILD_DIR%\lib
+if %CMAKE_GENERATOR%=="NMake Makefiles JOM" (
+    copy crypto\crypto.lib %BUILD_DIR%\lib
+    copy ssl\ssl.lib %BUILD_DIR%\lib
+) else (
+    copy crypto\%BUILD_TYPE%\crypto.lib %BUILD_DIR%\lib
+    copy ssl\%BUILD_TYPE%\ssl.lib %BUILD_DIR%\lib
+)
 xcopy /E /Q /Y include %BUILD_DIR%\include
 
 ENDLOCAL
