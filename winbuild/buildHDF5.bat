6fe20aad2fadf3b7b239c7ebea2e8014c78d2ee9
Alex Fuller <boberfly@gmail.com>
2018-05-26 11:13:19 -0700

    I had a lot of problems with this patch applying so I just skip it for now

--- winbuild/buildHDF5.bat
+++ winbuild/buildHDF5.bat
@@ -16,8 +16,11 @@ if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
 cd %ROOT_DIR%
-%ROOT_DIR%\winbuild\patch\bin\patch -f -p1 < %ROOT_DIR%\winbuild\hdf5_patch_1.diff
-%ROOT_DIR%\winbuild\patch\bin\patch -f -p1 < %ROOT_DIR%\winbuild\hdf5_patch_2.diff
+rem del %ROOT_DIR%\hdf5-1.8.20\config\cmake_ext_mod\ConfigureChecks.cmake
+rem del %ROOT_DIR%\hdf5-1.8.20\config\cmake_ext_mod\HDF5Tests.c
+rem del %ROOT_DIR%\hdf5-1.8.20\src\H5Omtime.c
+rem %ROOT_DIR%\winbuild\patch\bin\patch -f -p1 < %ROOT_DIR%\winbuild\hdf5_patch_1.diff
+rem %ROOT_DIR%\winbuild\patch\bin\patch -f -p1 < %ROOT_DIR%\winbuild\hdf5_patch_2.diff
 
 cd %WORKING_DIR%
 
@@ -28,9 +31,9 @@ mkdir gafferBuild
 cd gafferBuild
 del /f CMakeCache.txt
 
-cmake -C ..\config\cmake\cacheinit.cmake -Wno-dev -G %CMAKE_GENERATOR% -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% -DHDF5_ENABLE_THREADSAFE=ON -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DHDF5_BUILD_EXAMPLES=OFF -DHDF5_BUILD_FORTRAN=OFF -DHDF5_ENABLE_SZIP_SUPPORT=OFF ..
+cmake -C ..\config\cmake\cacheinit.cmake -Wno-dev -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%BUILD_TYPE% -DCMAKE_INSTALL_PREFIX=%BUILD_DIR% -DCMAKE_C_COMPILER_WORKS=1 -DCMAKE_CXX_COMPILER_WORKS=1 -DHDF5_ENABLE_THREADSAFE=ON -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DHDF5_BUILD_EXAMPLES=OFF -DHDF5_BUILD_FORTRAN=OFF -DHDF5_ENABLE_SZIP_SUPPORT=OFF ..
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
-cmake --build . --config %BUILD_TYPE% --target install
+cmake --build . --config %BUILD_TYPE% --target install -- -j %NUMBER_OF_PROCESSORS%
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
 ENDLOCAL
