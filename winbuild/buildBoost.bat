d42db31648d03bc15a4e9ae999af617370519249
Alex Fuller <boberfly@gmail.com>
2018-05-26 11:11:08 -0700

    So that Boost picks up the cl.exe compiler from %PATH%

--- winbuild/buildBoost.bat
+++ winbuild/buildBoost.bat
@@ -11,8 +11,9 @@ set PATH=%BUILD_DIR%\bin;%PATH%
 call bootstrap.bat --prefix=%BUILD_DIR% --with-python=%BUILD_DIR% --with-python-root=%BUILD_DIR% --without-libraries=log
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 set "BUILD_DIR=%BUILD_DIR:\=\\%"
-echo "using python ^: 2.7 : %BUILD_DIR% ^: %BUILD_DIR%\include ^: %BUILD_DIR%\lib ^: ^<address-model^>64 ^; >> project-config.jam"
-b2 --prefix=%BUILD_DIR% --toolset=%BOOST_MSVC_VERSION% architecture=x86 address-model=64 --build-type=complete variant=release link=shared threading=multi -s ZLIB_SOURCE=%ROOT_DIR%\zlib-1.2.11 -s ZLIB_INCLUDE=%BUILD_DIR%\include -s ZLIB_LIBPATH=%BUILD_DIR%\lib -s ZLIB_BINARY=zlib install
+rem echo "using python ^: 2.7 : %BUILD_DIR% ^: %BUILD_DIR%\include ^: %BUILD_DIR%\lib ^: ^<address-model^>64 ^; >> project-config.jam"
+copy  %ROOT_DIR%\winbuild\project-config.jam project-config.jam
+b2 --prefix=%BUILD_DIR% --toolset=%BOOST_MSVC_VERSION% architecture=x86 address-model=64 --build-type=complete variant=release link=shared threading=multi -s ZLIB_SOURCE=%ROOT_DIR%\zlib-1.2.11 -s ZLIB_INCLUDE=%BUILD_DIR%\include -s ZLIB_LIBPATH=%BUILD_DIR%\lib -s ZLIB_BINARY=zlib -j %NUMBER_OF_PROCESSORS% install
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 set "BUILD_DIR=%BUILD_DIR:\\=\%"
 
