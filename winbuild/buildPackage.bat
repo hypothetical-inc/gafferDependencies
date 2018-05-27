350131ca1d289cbc2e82e7a87c4bbdd6ff9aebfa
Alex Fuller <boberfly@gmail.com>
2018-05-26 20:07:27 -0700

    Relocating build package

--- winbuild/buildPackage.bat
+++ winbuild/buildPackage.bat
@@ -3,7 +3,7 @@ SETLOCAL
 set VERSION=0.46.0.0
 
 cd %BUILD_DIR%
-%ROOT_DIR%\winbuild\7zip\7za.exe a -tzip %ROOT_DIR%\gafferDependencies-%VERSION%-windows-msvc2017.zip @%ROOT_DIR%\winbuild\packageList.txt
+%ROOT_DIR%\winbuild\7zip\7za.exe a -tzip %BUILD_DIR%\gafferDependencies-%VERSION%-windows.zip @%ROOT_DIR%\winbuild\packageList.txt
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
 ENDLOCAL
