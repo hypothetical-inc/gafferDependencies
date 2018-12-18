d75644b0eb0968f30f58e828799ad0d80ef858a5
Eric Mehl <eric@thinkhypothetical.com>
2018-12-18 13:24:34 -0500

    PyOpenGL : add Windows configuration

--- PyOpenGL/config.py
+++ PyOpenGL/config.py
@@ -31,6 +31,16 @@
 		"python/OpenGL",
 
 	],
+	
+	"platform:windows" : {
+
+		"commands" : [
+
+			"{buildDir}\\bin\\python setup.py install --prefix {buildDir} --install-lib {buildDir}\\python",
+
+		]
+
+	},
 
 	"variables" : {
 
