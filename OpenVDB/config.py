a1937cbbf2792b58749c9b54b5bbf4a936b9b8ab
Eric Mehl <eric@thinkhypothetical.com>
2019-07-24 17:56:33 -0400

    OpenVDB : add Windows configuration

--- OpenVDB/config.py
+++ OpenVDB/config.py
@@ -40,9 +40,48 @@
 
 		"include/openvdb",
 		"include/pyopenvdb.h",
-		"lib/libopenvdb*{sharedLibraryExtension}*",
+		"lib/{libraryPrefix}openvdb*{sharedLibraryExtension}*",
 		"python/pyopenvdb*",
 
 	],
+
+	"platform:windows" : {
+
+		"variables" : {
+		
+			"cmakeGenerator" : "\"Visual Studio 15 2017 Win64\"",
+		},
+
+		"commands" : [
+			# OpenVDB requests Python 2.7 specifically but Boost doesn't add version numbers until v1.67
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -Wno-dev"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D OPENVDB_BUILD_UNITTESTS=OFF"
+				" -D OPENVDB_BUILD_DOCS=OFF"
+				" -D OPENVDB_BUILD_PYTHON_MODULE=ON"
+				" -D USE_GLFW3=OFF"
+				" -D TBB_LOCATION={buildDir}"
+				" -D BOOST_ROOT={buildDir}"
+				" -D GLEW_LOCATION={buildDir}"
+				" -D ILMBASE_LOCATION={buildDir}"
+				" -D OPENEXR_LOCATION={buildDir}"
+				" -D BLOSC_LOCATION={buildDir}"
+				" -D OPENVDB_ENABLE_3_ABI_COMPATIBLE=OFF"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install",
+			# v7.0.0 names the Python library incorrectly
+			# v7.1 fixes this, when updating this line should change the extension to .pyd
+			# for both source and destination
+			"copy {buildDir}\\lib\\python2.7\\pyopenvdb.dll {buildDir}\\python\\pyopenvdb.pyd"
+
+		],
+
+	},
 
 }
