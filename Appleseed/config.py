GafferHQ/dependencies

Appleseed : add Windows configuration

Appleseed/config.py
@@ -38,12 +38,15 @@
 
 		"cd build &&"
 			" cmake"
+			" -G {cmakeGenerator}"
+			" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
 			" -D WITH_CLI=ON"
 			" -D WITH_STUDIO=OFF"
 			" -D WITH_TOOLS=OFF"
 			" -D WITH_TESTS=OFF"
 			" -D WITH_PYTHON=ON"
 			" -D USE_STATIC_BOOST=OFF"
+			" -D USE_STATIC_EXR=OFF"
 			" -D USE_STATIC_OIIO=OFF"
 			" -D USE_STATIC_OSL=OFF"
 			" -D USE_EXTERNAL_ZLIB=ON"
@@ -58,8 +61,7 @@
 			" -D CMAKE_INSTALL_PREFIX={buildDir}/appleseed"
 			" -D PYTHON_INCLUDE_DIR={pythonIncludeDir}"
 			" ..",
-
-		"cd build && make install -j {jobs} VERBOSE=1"
+		"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
 
 	],
 
@@ -73,5 +75,68 @@
 		},
 
 	},
+
+	"platform:windows" : {
+
+		"variables" : {
+			"cmakeGenerator" : "\"Visual Studio 15 2017 Win64\"",
+		},
+
+		"environment" : {
+
+			"PATH" : "%PATH%;{buildDir}\\lib;{buildDir}\\bin",
+
+		},
+
+		"commands" : [
+			"if not exist \"build\" mkdir build",
+			"cd build &&"
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D PYTHON_LIBRARY={buildDir}\\lib\\python27.lib"
+				" -D PYTHON_INCLUDE_DIR={buildDir}\\include"
+				" -D WITH_OSL=ON"
+				" -D WITH_CLI=ON"
+				" -D WITH_STUDIO=OFF"
+				" -D WITH_TOOLS=OFF"
+				" -D WITH_PYTHON=ON"
+				" -D WITH_OSL=ON"
+				" -D WITH_TESTS=OFF"
+				" -D USE_STATIC_BOOST=OFF"
+				" -D USE_STATIC_OIIO=OFF"
+				" -D USE_STATIC_EXR=OFF"
+				" -D USE_STATIC_OSL=OFF"
+				" -D USE_EXTERNAL_ZLIB=ON"
+				" -D USE_EXTERNAL_EXR=ON"
+				" -D USE_EXTERNAL_PNG=ON"
+				" -D USE_EXTERNAL_XERCES=ON"
+				" -D USE_EXTERNAL_OSL=ON"
+				" -D USE_EXTERNAL_OIIO=ON"
+				" -D USE_EXTERNAL_ALEMBIC=ON"
+				" -D WARNINGS_AS_ERRORS=OFF"
+				" -D USE_SSE=ON"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}\\appleseed"
+				" -D BOOST_ROOT={buildDir}"
+				" -D IMATH_INCLUDE_DIR={buildDir}\\include"
+				" -D IMATH_HALF_LIBRARY={buildDir}\\lib\\Half.lib"
+				" -D IMATH_IEX_LIBRARY={buildDir}\\lib\\Iex-2_2.lib"
+				" -D IMATH_MATH_LIBRARY={buildDir}\\lib\\Imath-2_2.lib"
+				" -D OPENEXR_INCLUDE_DIR={buildDir}\\include"
+				" -D OPENEXR_IMF_LIBRARY={buildDir}\\lib\\IlmImf-2_2.lib"
+				" -D OPENEXR_THREADS_LIBRARY={buildDir}\\lib\\IlmThread-2_2.lib"
+				" -D XERCES_LIBRARY={buildDir}\\lib\\xerces-c_3.lib"
+				" -D OSL_INCLUDE_DIR={buildDir}\\include"
+				" -D LLVM_LIBS_DIR={buildDir}\\lib"
+				" OSL_EXEC_LIBRARY={buildDir}\\lib\\oslexec.lib"
+				" OSL_COMP_LIBRARY={buildDir}\\lib\\oslcomp.lib"
+				" OSL_QUERY_LIBRARY={buildDir}\\lib\\oslquery.lib"
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install",
+		]
+
+	}
 
 }

hypothetical-inc/gafferDependencies

Appleseed/config.py
@@ -38,12 +38,15 @@
 
 		"cd build &&"
 			" cmake"
+			" -G {cmakeGenerator}"
+			" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
 			" -D WITH_CLI=ON"
 			" -D WITH_STUDIO=OFF"
 			" -D WITH_TOOLS=OFF"
 			" -D WITH_TESTS=OFF"
 			" -D WITH_PYTHON=ON"
 			" -D USE_STATIC_BOOST=OFF"
+			" -D USE_STATIC_EXR=OFF"
 			" -D USE_STATIC_OIIO=OFF"
 			" -D USE_STATIC_OSL=OFF"
 			" -D USE_EXTERNAL_ZLIB=ON"
@@ -58,8 +61,7 @@
 			" -D CMAKE_INSTALL_PREFIX={buildDir}/appleseed"
 			" -D PYTHON_INCLUDE_DIR={pythonIncludeDir}"
 			" ..",
-
-		"cd build && make install -j {jobs} VERBOSE=1"
+		"cd build && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}"
 
 	],
 
@@ -73,5 +75,68 @@
 		},
 
 	},
+
+	"platform:windows" : {
+
+		"variables" : {
+			"cmakeGenerator" : "\"Visual Studio 15 2017 Win64\"",
+		},
+
+		"environment" : {
+
+			"PATH" : "%PATH%;{buildDir}\\lib;{buildDir}\\bin",
+
+		},
+
+		"commands" : [
+			"if not exist \"build\" mkdir build",
+			"cd build &&"
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_VERBOSE_MAKEFILE:BOOL=ON"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D PYTHON_LIBRARY={buildDir}\\lib\\python27.lib"
+				" -D PYTHON_INCLUDE_DIR={buildDir}\\include"
+				" -D WITH_OSL=ON"
+				" -D WITH_CLI=ON"
+				" -D WITH_STUDIO=OFF"
+				" -D WITH_TOOLS=OFF"
+				" -D WITH_PYTHON=ON"
+				" -D WITH_OSL=ON"
+				" -D WITH_TESTS=OFF"
+				" -D USE_STATIC_BOOST=OFF"
+				" -D USE_STATIC_OIIO=OFF"
+				" -D USE_STATIC_EXR=OFF"
+				" -D USE_STATIC_OSL=OFF"
+				" -D USE_EXTERNAL_ZLIB=ON"
+				" -D USE_EXTERNAL_EXR=ON"
+				" -D USE_EXTERNAL_PNG=ON"
+				" -D USE_EXTERNAL_XERCES=ON"
+				" -D USE_EXTERNAL_OSL=ON"
+				" -D USE_EXTERNAL_OIIO=ON"
+				" -D USE_EXTERNAL_ALEMBIC=ON"
+				" -D WARNINGS_AS_ERRORS=OFF"
+				" -D USE_SSE=ON"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}\\appleseed"
+				" -D BOOST_ROOT={buildDir}"
+				" -D IMATH_INCLUDE_DIR={buildDir}\\include"
+				" -D IMATH_HALF_LIBRARY={buildDir}\\lib\\Half.lib"
+				" -D IMATH_IEX_LIBRARY={buildDir}\\lib\\Iex-2_3.lib"
+				" -D IMATH_MATH_LIBRARY={buildDir}\\lib\\Imath-2_3.lib"
+				" -D OPENEXR_INCLUDE_DIR={buildDir}\\include"
+				" -D OPENEXR_IMF_LIBRARY={buildDir}\\lib\\IlmImf-2_3.lib"
+				" -D OPENEXR_THREADS_LIBRARY={buildDir}\\lib\\IlmThread-2_3.lib"
+				" -D XERCES_LIBRARY={buildDir}\\lib\\xerces-c_3.lib"
+				" -D OSL_INCLUDE_DIR={buildDir}\\include"
+				" -D LLVM_LIBS_DIR={buildDir}\\lib"
+				" OSL_EXEC_LIBRARY={buildDir}\\lib\\oslexec.lib"
+				" OSL_COMP_LIBRARY={buildDir}\\lib\\oslcomp.lib"
+				" OSL_QUERY_LIBRARY={buildDir}\\lib\\oslquery.lib"
+				" ..",
+			"cd build && cmake --build . --config {cmakeBuildType} --target install",
+		]
+
+	}
 
 }