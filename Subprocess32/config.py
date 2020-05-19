7a91bbb18266e773750a29f92b22ee7798571433
Eric Mehl <eric@thinkhypothetical.com>
2020-05-19 12:09:35 -0400

    subprocess32 : add manifest

--- Subprocess32/config.py
+++ Subprocess32/config.py
@@ -28,6 +28,13 @@
 
 	],
 
+	"manifest" : [
+
+		"lib/python{pythonVersion}/site-packages/subprocess32.py",
+		"lib/python{pythonVersion}/site-packages/_posixsubprocess.so",
+
+	],
+
 	"platform:windows" : {
 
 		"enabled" : False,
