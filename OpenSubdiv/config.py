49205edb091dbf87dfba2534e5d572b491acd59a
Eric Mehl <eric@thinkhypothetical.com>
2020-10-16 14:17:31 -0400

    OpenSubdiv : add Windows configuration

--- OpenSubdiv/config.py
+++ OpenSubdiv/config.py
@@ -13,6 +13,7 @@
 	"commands" : [
 
 		"cmake"
+			" -G {cmakeGenerator}"
 			" -D CMAKE_INSTALL_PREFIX={buildDir}"
 			" -D CMAKE_PREFIX_PATH={buildDir}"
 			" -D NO_DOC=1"
@@ -28,8 +29,7 @@
 			" ."
 		,
 
-		"make VERBOSE=1 -j {jobs}",
-		"make install",
+		"cmake --build . --config {cmakeBuildType} --target install -- VERBOSE=1 -j {jobs}",
 
 	],
 
@@ -39,5 +39,36 @@
 		"lib/libosd*{sharedLibraryExtension}*",
 
 	],
+
+	"platform:windows" : {
+
+		"variables" : {
+			"cmakeGenerator" : "\"Visual Studio 15 2017 Win64\"",
+		},
+
+		"commands" : [
+
+			"cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D CMAKE_PREFIX_PATH={buildDir}"
+				" -D NO_DOC=1"
+				" -D NO_OMP=1"
+				" -D NO_CUDA=1"
+				" -D NO_OPENCL=1"
+				" -D NO_GLEW=1"
+				" -D NO_GLFW=1"
+				" -D NO_DX=1"
+				" -D NO_TESTS=1"
+				" -D NO_TBB=1"
+				" -D OPENEXR_LOCATION={buildDir}/lib"
+				" ."
+			,
+
+			"cmake --build . --config {cmakeBuildType} --target install",
+
+	],
+
+	},
 
 }
