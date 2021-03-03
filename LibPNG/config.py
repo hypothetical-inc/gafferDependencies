GafferHQ/dependencies

LibPNG : add Windows configuration

LibPNG/config.py
@@ -16,4 +16,24 @@
 
 	],
 
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D ZLIB_INCLUDE_DIR={buildDir}\\include"
+				" -D ZLIB_LIBRARY={buildDir}\\lib\\zlibstatic.lib"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+
+		],
+
+	},
+
 }

hypothetical-inc/gafferDependencies

LibPNG/config.py
@@ -21,8 +21,27 @@
 
 		"include/png*",
 		"include/libpng*",
-		"lib/libpng*{sharedLibraryExtension}*",
+		"lib/libpng*{sharedLibraryExtension}*",	# lib prefix is accurate for all platforms
 
 	],
+	"platform:windows" : {
+
+		"commands" : [
+
+			"mkdir gafferBuild",
+			"cd gafferBuild && "
+				" cmake"
+				" -G {cmakeGenerator}"
+				" -D CMAKE_BUILD_TYPE={cmakeBuildType}"
+				" -D CMAKE_INSTALL_PREFIX={buildDir}"
+				" -D ZLIB_INCLUDE_DIR={buildDir}\\include"
+				" -D ZLIB_LIBRARY={buildDir}\\lib\\zlib.lib"
+				" ..",
+
+			"cd gafferBuild && cmake --build . --config {cmakeBuildType} --target install -- -j {jobs}",
+
+		],
+
+	},
 
 }